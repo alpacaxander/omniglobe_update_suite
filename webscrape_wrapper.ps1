268ee7b211cf06217dd072a7b91079839795bbc3
Alexander Paulsell <apaulsell@csumb.edu>
2018-08-10 14:12:50 -0700

    various changes to script and scheduler

--- webscrape_wrapper.ps1
+++ webscrape_wrapper.ps1
@@ -18,4 +18,19 @@ $LOCAL_IR = "C:\RT_Contents\NRL_IR"
 $DOCKER_IR = "/dockercylc/webscrape/ir"
 $LOCAL_SUITE = "C:\cylc\webscrape"
 $DOCKER_SUITE = "/dockercylc/webscrape"
-docker run -it -v ${LOCAL_RELVOR}:${DOCKER_RELVOR} -v ${LOCAL_SOL_RAD}:${DOCKER_SOL_RAD} -v ${LOCAL_IR}:${DOCKER_IR} -v ${LOCAL_WW}:${DOCKER_WW} -v ${LOCAL_MSLP}:${DOCKER_MSLP} -v ${LOCAL_10M}:${DOCKER_10M} -v ${LOCAL_WV}:${DOCKER_WV} -v ${LOCAL_AEROSOLS}:${DOCKER_AEROSOLS} -v ${LOCAL_IRBM}:${DOCKER_IRBM} -v ${LOCAL_SUITE}:${DOCKER_SUITE} -e "suitename=webscrape" -e "coldstart=1" -p 5800:5800 cylc
+
+$COLD_START = "coldstart=1"
+$SUITE_NAME = "suitename=webscrape"
+
+md ${LOCAL_WW} -ErrorAction SilentlyContinue
+md ${LOCAL_MSLP} -ErrorAction SilentlyContinue
+md ${LOCAL_10M} -ErrorAction SilentlyContinue
+md ${LOCAL_RELVOR} -ErrorAction SilentlyContinue
+md ${LOCAL_SOL_RAD} -ErrorAction SilentlyContinue
+md ${LOCAL_WV} -ErrorAction SilentlyContinue
+md ${LOCAL_AEROSOLS} -ErrorAction SilentlyContinue
+md ${LOCAL_IRBM} -ErrorAction SilentlyContinue
+md ${LOCAL_IR} -ErrorAction SilentlyContinue
+md ${LOCAL_SUITE} -ErrorAction SilentlyContinue
+
+docker run -d -v ${LOCAL_RELVOR}:${DOCKER_RELVOR} -v ${LOCAL_SOL_RAD}:${DOCKER_SOL_RAD} -v ${LOCAL_IR}:${DOCKER_IR} -v ${LOCAL_WW}:${DOCKER_WW} -v ${LOCAL_MSLP}:${DOCKER_MSLP} -v ${LOCAL_10M}:${DOCKER_10M} -v ${LOCAL_WV}:${DOCKER_WV} -v ${LOCAL_AEROSOLS}:${DOCKER_AEROSOLS} -v ${LOCAL_IRBM}:${DOCKER_IRBM} -v ${LOCAL_SUITE}:${DOCKER_SUITE} -e ${SUITE_NAME} -e ${COLD_START} -p 5800:5800 cylc
